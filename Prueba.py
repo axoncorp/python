ada609255368368fd8d4c88cb6d6ceba3c9211bd
axoncorp <proyectos@lp-asociados.com>
2024-10-03 16:50:13 -0500

    Actualizado prueba1

---
+++ Prueba.py
@@ -0,0 +1,8 @@
+#Calcular la velocidad promedio de un vehiculo
+#Formula: v=d(distancia-km)/t(tiempo-hr)
+
+d=float(input("Ingrese la distancia a recorrer:"))
+t=float(input("Ingrese el tiempo estimado:"))
+v=d/t
+
+print("La velocidad promedio del vehiculo es:",v)
